06370c0a695298cb77705e5f3af207ad9982957f
Hel <borealissa@gmail.com>
2020-04-27 00:47:54 +0300

    js trouble

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,22 @@ $(document).ready(function(){
   $('.main-nav').removeClass('main-nav--no-js');
   $('.main-nav').addClass('main-nav--closed');
 
-  $('.main-nav__button-close').click(function(){
-     if ($('.main-nav').hasClass('main-nav--closed')){
+
+  let openMainMenu = function(){
       $('.main-nav').addClass('main-nav--opened');
       $('.main-nav').removeClass('main-nav--closed');
+  };
+
+  let closeMainMenu = function(){
+    $('.main-nav').addClass('main-nav--closed');
+    $('.main-nav').removeClass('main-nav--opened');
+  };
+
+  $('.main-nav__button-close').click(function(){
+     if ($('.main-nav').hasClass('main-nav--closed')){
+      openMainMenu();
     } else {
-      $('.main-nav').addClass('main-nav--closed');
-      $('.main-nav').removeClass('main-nav--opened');
+      closeMainMenu();
     };
 
     $('body').toggleClass('lock');
@@ -23,16 +32,17 @@ $(document).ready(function(){
 
   $('.page-filter__hidden-btn').on('click', function(){
     $('.page-filter__wrapper').toggleClass('page-filter__block--active');
-    $('body').toggleClass('lock');
+    $('body').addClass('lock');
   });
 
   $('.modal-enter__button-close').on('click', function(){
     $('.page-filter__wrapper, .page-filter__aside-list').removeClass('page-filter__block--active');
     $('.modal-overlay').removeClass('modal-overlay__show');
+    $('body').removeClass('lock');
   });
 
 
-  $('.header-phones__button').magnificPopup({
+  $('.js-callback').magnificPopup({
     type: 'inline',
     focus: '#callback',
     closeBtnInside: false,
@@ -43,17 +53,18 @@ $(document).ready(function(){
     }
   });
 
-  $('.user-list__button').magnificPopup({
+  $('.js-singup').magnificPopup({
     type: 'inline',
     focus: '#singup_email',
     closeBtnInside: false,
   });
 
   $(document).on('click','.modal-enter__button-close', function(){
+    closeMainMenu();
     $.magnificPopup.close();
   });
 
-   $('.form-validate').each(function(){
+   $('.js-form-validate').each(function(){
   let $form = $(this);
   $form.validate({
     rules: {
@@ -64,6 +75,14 @@ $(document).ready(function(){
       password: {
         required: true,
         minlength: 4
+      },
+      search: {
+        minlength: 2
+      },
+      cost: {
+        number: true,
+        min: 50,
+        step: 1
       }
     },
     messages: {
@@ -72,13 +91,30 @@ $(document).ready(function(){
       },
       email: {
         required: 'Поле email обязательно к заполнению',
-        email: 'Введите, корректный email'
+        email: 'Введите корректный email'
       }, 
       password: {
         required: 'Поле "Пароль" обязательно к заполнению',
         minlength: 'Пароль должен состоять минимум из 4 символов'
-    }
+    },
+      search: {
+        minlength: 'Запрос должен состоять минимум из 2 символов'
+      },
+      cost: 'Введите число больше 50'
   }
     });
  });
+    
+   let clicks = 0;
+   let addCounter = function(){
+      $('.actions-list__counter').show('.actions-list__link');
+      clicks++;
+      $('.actions-list__counter').html(clicks);
+    };
+
+    $('.js-item-compare').on('click', function(){
+      $('.js-title-compare').text('В сравнении');
+        addCounter();
+    });
+
 });
